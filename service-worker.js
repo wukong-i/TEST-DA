0b3dd5008772065d9d38e17a1196588b369aa899
wukong-i <deavidni@gmail.com>
2024-11-22 22:25:57 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('static-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/icon-192x192.png',
+        '/icon-512x512.png',
+        '/styles.css', // Add your CSS and other files
+        '/script.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
